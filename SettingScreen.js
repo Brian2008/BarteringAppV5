3901589b30cfa191b755bedd22bf5cc2bf4bd43d
Brian2008 <71983333+Brian2008@users.noreply.github.com>
2021-06-04 23:59:35 -0700

    Add files via upload

---
+++ SettingScreen.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity, Touchable} from 'react-native';
+import {DrawerItems} from 'react-navigation-drawer';
+
+import firebase from 'firebase';
+
+export default class settingScreen extends Component{
+    render(){
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
